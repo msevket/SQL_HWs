f6ea021557a122344bb3e7ade35d5a49a1598cb6
msevket <79874540+msevket@users.noreply.github.com>
2023-03-17 19:18:40 +0300

    Update SQL_HW6.sql

--- SQL_HW6.sql
+++ SQL_HW6.sql
@@ -13,16 +13,14 @@ Sonuç=92
 
 3.film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
 
-SELECT length from film
-WHERE rental_rate=0.99
-ORDER BY length DESC
-LIMIT 1;
+SELECT MAX(length) from film
+WHERE rental_rate=0.99;
 
 Sonuç=184
 
 4.film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 
-SELECT DISTINCT(COUNT(replacement_cost)) from film
+SELECT COUNT(DISTINCT(replacement_cost)) from film
 WHERE length>150;
 
-Sonuç=242
+Sonuç=21
